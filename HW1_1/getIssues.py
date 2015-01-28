be09aec24d71e166ae1480375a28e2e9e6443572
Rahul Krishna <i.m.ralk@gmail.com>
2015-01-28 17:07:20 -0500

    stuff

--- HW1_1/getIssues.py
+++ HW1_1/getIssues.py
@@ -1,17 +1,22 @@
 """ Code used to extract data from GITHUB"""
-
+from __future__ import print_function
 from pygithub3 import Github
 import time
 import datetime
-def d2s(x):
-  temp =time.mktime(time.strptime(str(x), "%Y-%m-%d %H:%M:%S"))
-  return (time.time() - temp)/(3600*24)
 
-auth = dict(login='rahlk', password='nb20152dq')
+def d2s(x):
+  temp = time.mktime(time.strptime(str(x), "%Y-%m-%d %H:%M:%S"))
+  return (time.time() - temp) / (3600 * 24)
+
+auth = dict(login = 'rahlk', password = 'nb20152dq')
 gh = Github(**auth)
 
-octocat_issues = gh.issues.list_by_repo('scikit-learn','scikit-learn',state="all")
-
-for page in enumerate(octocat_issues):
-  for resource in page:
-    print "State: ",resource.state," Days: ",d2s(resource.created_at)#," closed at: ",d2s(resource.closed_at)
+octocat_issues = gh.issues.list_by_repo('scikit-learn', 'scikit-learn', state = "all")
+indx = 0
+print('Index, State, Days, Rate')
+for page in (octocat_issues):
+  for resource in (page):
+    indx += 1
+    now = d2s(resource.created_at)
+    if indx % 50 == 0:
+      print('{0:1d},{1:6s},{2:}'.format(indx , resource.state, now))  # ," closed at: ",d2s(resource.closed_at)
